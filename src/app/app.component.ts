d42fd9adcdb0ef7b3f8168ba0f89b3c1a6a18dd1
Sarvesh <sarvesh@cardinality.ai>
2021-11-15 15:18:33 +0530

    login functionality deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-deploy';
+  title = 'demo-app';
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler=new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+  login(){
+    this.oauthService.initImplicitFlow();
+  }
+  logout(){
+    this.oauthService.logOut();
+  }
+
+  get token(){
+    let claims:any=this.oauthService.getIdentityClaims();
+    return claims ? claims:null;
+  }
 }
